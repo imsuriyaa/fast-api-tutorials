77ed3a2f360d1a8903bb05fdfdef8ab205e8a5f3
imsuriyaa <suriyaa.iitkgp@gmail.com>
2025-11-06 01:55:33 +0530

    Complete CRUD operations in the app

--- Project 1/books.py
+++ Project 1/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -57,3 +57,26 @@ async def read_book_by_author_and_category(book_author: str, category: str):
 # async is fairly optional for 
 async def hello_world():
     return {"message": "Hello World!"}
+
+
+@app.post('/books/create_book')
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+    return BOOKS
+
+@app.put('/books/update_book')
+async def update_book(updated_book = Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+    return BOOKS
+
+@app.delete('/books/delete/{book_title}')
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+    return BOOKS
+
+
+
