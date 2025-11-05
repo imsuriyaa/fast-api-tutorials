d85f5306b5fd01decdc420b6d137244fd529b423
imsuriyaa <suriyaa.iitkgp@gmail.com>
2025-11-06 01:15:10 +0530

    Implement GET with Query & Path Params

--- Project 1/books.py
+++ Project 1/books.py
@@ -16,12 +16,41 @@ BOOKS = [
 async def read_all_books():
     return BOOKS
 
-@app.get('/books/{book_title}')
-async def read_all_books(book_tile):
+# Path Parameters
+@app.get('/books/title/{book_title}')
+async def read_all_books(book_title):
     for book in BOOKS:
-        if book['title'] == book_tile:
+        if book['title'].casefold() == book_title.casefold():
             return book
-    
+    return {"message": f"Book with title {book_title} not found"}
+
+# Query Parameter
+@app.get('/books/')
+async def read_books_by_category(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    if books_to_return:
+        return books_to_return
+    else:
+        return {'messgae': f"Books in category: {category} not found"}
+
+
+# Filtering based on both Path and Query Parameter
+@app.get('/books/author/{book_author}')
+async def read_book_by_author_and_category(book_author: str, category: str):
+    books_to_return = []
+    print("Inside Read Book by Author and Category")
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and book.get('category') == category.casefold():
+            books_to_return.append(book)
+        
+    if books_to_return:
+        return books_to_return
+    else:
+        return {'messgae': f"Book not found of the category: {category} by the author {book_author}"}
 
 
 @app.get('/hello-world')
