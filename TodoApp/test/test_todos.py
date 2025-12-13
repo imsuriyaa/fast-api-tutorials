1f1fb3b43f2ce928df55e26548576766014e2519
imsuriyaa <suriyaa.iitkgp@gmail.com>
2025-12-13 22:58:42 +0530

    Implement unit testing for getting a todo

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -66,3 +66,16 @@ def test_read_all_authenticated(test_todo):
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == [{'id': 1, 'title': 'Learn to code!','description': 'Need to learn everyday!','priority': 5,'complete': False,'owner_id': 1}]
 
+
+def test_read_one_authenticated(test_todo):
+    response = client.get('/todo/1')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {'id': 1, 'title': 'Learn to code!','description': 'Need to learn everyday!','priority': 5,'complete': False,'owner_id': 1}
+
+
+def test_read_one_authenticated_not_found(test_todo):
+    response = client.get('/todo/999')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {'detail': 'Todo not found'}
+
+
